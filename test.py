a1d16e2d9ad2e8f7c64b3ddb4169a4f438f2764b
deeptijain1 <deepti.jain1@tcs.com>
2016-10-05 16:46:55 +0530

    Python_code

---
+++ test.py
@@ -0,0 +1,18 @@
+from Tkinter import *
+top=Tk()
+#top1=Tk()
+Label(top, text="UserId").grid(row=0)
+Label(top, text="User Name").grid(row=1)
+Label(top, text="Domain").grid(row=2)
+Label(top, text="Role").grid(row=3)
+e1=Entry(top,bd=5)
+e2=Entry(top,bd=5)
+e3=Entry(top,bd=5)
+e4=Entry(top,bd=5)
+
+e1.grid(row=0,column=1)
+e2.grid(row=1,column=1)
+e3.grid(row=2,column=1)
+e4.grid(row=3,column=1)
+# print ("hello python")
+top.mainloop()
